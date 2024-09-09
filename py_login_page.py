e45f6e872ae8c0dc757ab8f1c1322886ef66ed2c
Keshab Dahal <102454190+Ksub2@users.noreply.github.com>
2024-09-09 19:06:10 +0545

    Update py_login_page.py

--- py_login_page.py
+++ py_login_page.py
@@ -5,11 +5,9 @@ from tkinter import messagebox
 import mysql.connector
 from py_mainform import mainform
 
-
 root = Tk()
 connection=mysql.connector.connect(host='localhost',user='root',port='3306',password='',database='py_lg_rb_db')
 c=connection.cursor()
-# Set window title and size
 #root.title("Login")
 #root.geometry("250x100") 
 w=450
@@ -18,9 +16,7 @@ h=525
 #background color
 bgcolor="#bdc3c7"
 
-
-
-#center form------
+#center form
 root.overrideredirect(1) #removes border
 ws= root.winfo_screenwidth()
 hs= root.winfo_screenheight()
@@ -39,8 +35,6 @@ titleframe.pack()
 title_label.pack()
 close_button.pack()
 
-
-
 titleframe.place(y=27, relx=0.5, anchor=CENTER)
 close_button.place(x=410,y=10)
 #close window----
@@ -48,15 +42,14 @@ def close_win():
     root.destroy()
 
 close_button['command'] = close_win
-
-# Create the main frame
+#  main frame
 mainframe = tk.Frame(root, width=w, height=h)
 
-# Create content frame (for username and password input)
+# content frame (for username and password input)
 loginframe=tk.Frame(mainframe,width=w,height=h)
 login_contentframe = tk.Frame(loginframe,padx=30,pady=100,highlightbackground='yellow',highlightcolor='yellow',highlightthickness=2, bg=bgcolor)
 
-# Create username and password labels and entry fields
+# username and password labels and entry fields
 username_label = tk.Label(login_contentframe, text='Username:',font=('Verdena',16),bg=bgcolor)
 password_label = tk.Label(login_contentframe, text='Password:',font=('Verdena',16),bg=bgcolor)
 
@@ -65,9 +58,7 @@ password_entry = tk.Entry(login_contentframe,font=('Verdena',16),show='*')  # Ma
 
 login_button= tk.Button(login_contentframe,text="Login", font=('Verdena',16),bg='#2980b9',fg='#fff',padx=25,pady=10,width=25)
 
-
 go_register_label = tk.Label(login_contentframe, text=">> Don't have an account? create new",font=('Verdena',10),bg=bgcolor,fg='red')
-
 
 # Packing frames
 mainframe.pack(fill="both", expand=True)
@@ -83,7 +74,7 @@ password_entry.grid(row=1, column=1)
 login_button.grid(row=2,column=0, columnspan=2,pady=40)
 go_register_label.grid(row=3,column=0, columnspan=2,pady=20)
 
-#creating a function to display the Register frame
+#function to display the Register frame
 
 def go_to_register():
     loginframe.forget()
@@ -93,7 +84,7 @@ def go_to_register():
 
 go_register_label.bind("<Button-1>", lambda page: go_to_register())
 
-#creating a function to make a user login
+# function to make a user login
 
 def login():
     username=username_entry.get().strip()
@@ -113,15 +104,11 @@ def login():
 
 
 login_button['command']= login
-
-
-
-
-#register page-----
+#register page
 registerframe=tk.Frame(mainframe,width=w,height=h)
 register_contentframe = tk.Frame(registerframe,padx=15,pady=15,highlightbackground='yellow',highlightcolor='yellow',highlightthickness=2, bg=bgcolor)
 
-# Creating username and password labels and entry fields
+#  username and password labels and entry fields
 fullname_label_rg = tk.Label(register_contentframe, text='Fullname:',font=('Verdena',14),bg=bgcolor)
 username_label_rg = tk.Label(register_contentframe, text='Username:',font=('Verdena',14),bg=bgcolor)
 password_label_rg = tk.Label(register_contentframe, text='Password:',font=('Verdena',14),bg=bgcolor)
@@ -129,10 +116,6 @@ confirmpass_label_rg = tk.Label(register_contentframe, text='Confirm-Password:',
 phone_label_rg = tk.Label(register_contentframe, text='Phone:',font=('Verdena',14),bg=bgcolor)
 gender_label_rg = tk.Label(register_contentframe, text='Gender:',font=('Verdena',14),bg=bgcolor)
 image_label_rg = tk.Label(register_contentframe, text='Profile Photo:',font=('Verdena',14),bg=bgcolor)
-
-
-
-
 
 fullname_entry_rg = tk.Entry(register_contentframe,font=('Verdena',14),width=23)
 username_entry_rg = tk.Entry(register_contentframe,font=('Verdena',14),width=23)  # Mask the password entry
@@ -150,15 +133,11 @@ selectimage_frame=tk.Frame(register_contentframe,bg=bgcolor)
 selectimage_button=tk.Button(selectimage_frame,text='select image',bg='#fff')
 imagepath_label_rg=tk.Label(selectimage_frame,text='image path..',bg=bgcolor,width=23)
 
-
 image_entry_rg = tk.Entry(register_contentframe, show="*",font=('Verdena',14))  
 
 register_button= tk.Button(register_contentframe,text="Register", font=('Verdena',16),bg='#2980b9',fg='#fff',padx=25,pady=10,width=25)
 
-
 go_login_label = tk.Label(register_contentframe, text=">> Already have an account? sign in",font=('Verdena',10),bg=bgcolor,fg='red')
-
-
 # Packing frames
 #mainframe.pack(fill="both", expand=True)
 #registerframe.pack(fill="both",expand=True)
@@ -190,9 +169,6 @@ selectimage_frame.grid(row=6, column=1)
 selectimage_button.grid(row=0,column=0,padx=10,pady=10)
 imagepath_label_rg.grid(row=0,column=1)
 
-
-
-
 register_button.grid(row=7,column=0, columnspan=2,pady=20)
 go_login_label.grid(row=8,column=0, columnspan=2,pady=10)
 
@@ -201,9 +177,8 @@ def select_image():
     filename=filedialog.askopenfilename(initialdir ='/images',title="Select Profile Picture",filetypes=(("png images","*.png"),("jpg images","*.jpg")))
     imagepath_label_rg['text']=filename
 selectimage_button['command']=select_image
-#--------------#
- 
-#creating a function to display the login frame
+
+#function to display the login frame
 def go_to_login():
     registerframe.forget()
     loginframe.pack(fill="both",expand=1)
@@ -211,10 +186,8 @@ def go_to_login():
     title_label['bg']='#2980b9'
 
 go_login_label.bind("<Button-1>", lambda page: go_to_login())
-#--------------#
 
-#create a function to check if the username already exist or not
-
+# function to check if the username already exist or not
 def check_username(username):
     username=username_entry_rg.get().strip()
     vals=(username,)
@@ -226,12 +199,8 @@ def check_username(username):
     else:
         #messagebox.showwarning('Correct','correct username')
         return False
-
-
-
-
-
-#creating a function to register a new user
+        
+# function to register a new user
 def register():
 
     fullname=fullname_entry_rg.get().strip() #removes white space
@@ -241,9 +210,6 @@ def register():
     phone=phone_entry_rg.get().strip()
     gdr=gender.get()
     imgpath=imagepath_label_rg['text']
-
-
-
 
     if len(fullname) > 0 and len(username) > 0 and len(password) > 0 and len(phone) > 0 :
         if check_username(username) ==False:
@@ -260,7 +226,6 @@ def register():
     else:
         messagebox.showwarning('Empty Fields',' Make sure to enter all the information')
 
-
 register_button['command']=register
 
 """ 
@@ -272,6 +237,5 @@ phone_label_rg         phone_entry_rg
 gender_label_rg        
 image_label_rg        image_entry_rg """
 
-
-# Start the main loop
+#  main loop start
 root.mainloop()
